9b2a7aff524bd7ab9a66c22a39d9a7865d49d654
Michael Laurents <simonsluke134@gmail.com>
2019-08-05 21:36:24 +0900

    betabeta html

--- 1/dapps.js
+++ 1/dapps.js
@@ -4,11 +4,12 @@ var address;
 
 ethereum.enable();
 
-
-
 const getContract = async ()=>{
     var response = await fetch("./build/contracts/SimpleStorage.json");
     var json = await response.json();
     abi = json.abi;
-
+    address = json.networks[5777].address;
+    contract = web3.eth.contract(abi).at(address);
 }
+
+getContract();
